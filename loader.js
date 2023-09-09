17c170adf4c44c1ddb6ebbbb71ec33bb46351a00
Marius <munteanymarius@gmail.com>
2023-09-09 14:26:46 +0300

    create a shop and product page

--- loader.js
+++ loader.js
@@ -12,3 +12,23 @@ if (close) {
         nav.classList.remove('active')
     })
 }
+
+let MainImg = document.getElementById("MainImg");
+let smallimg = document.getElementsByClassName("smaill-img");
+
+smallimg[0].onclick = function() { 
+    MainImg.src = smallimg[0].src;
+
+    }
+smallimg[1].onclick = function() { 
+    MainImg.src = smallimg[1].src;
+        
+    }
+smallimg[2].onclick = function() { 
+    MainImg.src = smallimg[2].src;
+            
+    }
+smallimg[3].onclick = function() { 
+    MainImg.src = smallimg[3].src;
+            
+    }
