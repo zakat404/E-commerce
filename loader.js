973f8a41c974f53e1eafa165685b7c050afb85e0
Marius <elevmarius@list.ru>
2023-09-13 13:00:44 +0300

    endding

--- loader.js
+++ loader.js
@@ -1,17 +1,4 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const navbar = document.getElementById('navbar');
 
-if (bar) { 
-    bar.addEventListener('click', () => {
-        navbar.classList.add('active')
-    })
-}
-if (close) { 
-    close.addEventListener('click', () => {
-        navbar.classList.remove('active')
-    })
-}
 
 let MainImg = document.getElementById("MainImg");
 let smallimg = document.getElementsByClassName("smaill-img");
